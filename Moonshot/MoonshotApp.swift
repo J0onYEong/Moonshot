2bcd6f8f99aa5bf2efe7806c5e2662f5a63f8f00
choijun0 <cjy00050005@gmail.com>
2022-11-23 17:05:23 +0900

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by 최준영 on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
